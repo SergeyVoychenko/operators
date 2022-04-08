9f99b3d48c468ba3a47ec13002a7402c5e423c2e
Sergey Voychenko <s.vouchenko@gmail.com>
2022-04-08 20:26:15 +0300

    remade to classic if else

--- js/index.js
+++ js/index.js
@@ -2,11 +2,25 @@ const a = Number(prompt('Введите первое число'));
 const operator = prompt('Выберите оператор: + , - , / , * ');
 const b = Number(prompt('Введите второе число'));
 if (!isNaN(a) && !isNaN(b)) {
-  let sum = (operator === '+') ? a + b :
-    (operator === '-') ? a - b :
-      (operator === '/') ? a / b :
-        (operator === '*') ? a * b :
-          'введите корректно один из операторов: + , - , / , * ';
+  // можно тернальным оператором ? но код становиться плохо читабелен.
+  // let sum = (operator === '+') ? a + b :
+  //   (operator === '-') ? a - b :
+  //     (operator === '/') ? a / b :
+  //       (operator === '*') ? a * b :
+  //         'введите корректно один из операторов: + , - , / , * ';
+  // рекомендуеться использовать классический if
+  let sum;
+  if (operator === '+') {
+    sum = a + b;
+  } else if (operator === '-') {
+    sum = a - b;
+  } else if (operator === '/') {
+    sum = a / b;
+  } else if (operator === '*') {
+    sum = a * b;
+  } else {
+    sum = ('введите корректно один из операторов: + , - , / , * ');
+  }
   alert(`Ваш результат:  ${sum}`);
 } else {
   alert('Введите корректно ЧИСЛО!');
